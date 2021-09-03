6dc8661361516a5ea61dbb6367404f622157a2fe
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-09-03 14:46:38 +0200

    [FEATURE] filter out worse than best+x

--- src/filter_matches.cpp
+++ src/filter_matches.cpp
@@ -3,30 +3,46 @@
 template <class records_t, class strata_t>
 records_t consolidate_matches_for_read(records_t &records, strata_t x)
 {
-// TODO:
-		    // find optimal read in records
-		    // filter out all below best + x
     using namespace seqan3::literals;
 
     seqan3::sam_tag_dictionary dict{};
     using nm_tag_type = seqan3::sam_tag_type_t<"NM"_tag>;
     
     std::deque<int> edit_distances{};
-    
+   
     // gather all edit distances for one read
-    for (auto & record : records){
-	// TODO: don't copy
+    for (auto & record : records)
+    {
+	// TODO: does this make a copy?
         dict = record.tags();
-        // seqan3::debug_stream << record.id() << '\n';
         auto nm = dict.get<"NM"_tag>();
 	edit_distances.push_back(nm);
-        // seqan3::debug_stream << nm << '\n';
     }
 
     std::deque<int>::iterator it = std::min_element(edit_distances.begin(), edit_distances.end());
-    seqan3::debug_stream << *it << '\n';
+    int optimal_nm = *it;
+    seqan3::debug_stream << "optimal: " << optimal_nm << '\n';
 
-    records_t filtered_records;
+    /* should be possible using a view but didn't work
+    auto nm_filter = std::views::filter([&] (auto & record)
+    {
+        return record.tags().get<"NM"_tag>() <= optimal_nm + x;    
+    });
+    */
+
+    records_t filtered_records{};
+    for (auto & record : records)
+    {
+	seqan3::debug_stream << record.id() << '\t';
+	dict = std::move(record.tags());
+	auto nm = dict.get<"NM"_tag>();
+	seqan3::debug_stream << nm << '\t';
+	if (nm <= optimal_nm + x)
+	{
+            filtered_records.push_back(record);
+	}
+    }
+
     return filtered_records;
 }
 void run_consolidator(std::filesystem::path sam_file, std::filesystem::path out, uint8_t x)
@@ -43,27 +59,33 @@ void run_consolidator(std::filesystem::path sam_file, std::filesystem::path out,
     for (auto & record : fin)  // this will pass the record by reference (no copy)
     {
 	// initialize the read ID and records
-	if(isFirst){
+	if(isFirst)
+	{
 	    current_read_id = record.id();
 	    records.push_back(record);
 	    isFirst = false;
 	}
 	// for each after first
-	else {
+	else 
+	{
 	    // if within the same collation of read matches
-	    if (current_read_id == record.id()){
+	    if (current_read_id == record.id())
+	    {
 	        records.push_back(record);
 
 	    }
 	    // if moved to matches for next read
-            else {
-                if (records.size() == 1){
+            else 
+	    {
+                if (records.size() == 1)
+		{
 		    // write out record
 		    fout = records;
 		}
-		else {
-		    consolidate_matches_for_read(records, x);
-		    // TODO: write out filtered matches
+		else 
+		{
+		    auto filtered_records = consolidate_matches_for_read(records, x); // return pointer; no copy
+		    fout = filtered_records;
 		}
 	        current_read_id = record.id();
 	        records.clear();
